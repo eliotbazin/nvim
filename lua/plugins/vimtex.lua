caf06c732edee089b71e4d62b920adcaf146a41d
Eliot Bazin <eliot.bazin@hotmail.com>
2024-04-16 16:04:51 +0200

    feat: enable vimtex folding

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -63,65 +63,69 @@ return function()
     'Package hyperref Warning: Token not allowed in a PDF string',
     [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in paragraph at lines]],
   }
-  --     [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in paragraph at lines]],
-  -- }
-  -- vim.g.vimtex_fold_enabled = 1
-  -- vim.g.vimtex_fold_manual = 1
-  -- vim.g.vimtex_fold_types = {
-  --     cmd_addplot = {
-  --         cmds = { "addplot[+3]?" },
-  --     },
-  --     cmd_multi = {
-  --         cmds = {
-  --             "%(re)?new%(command|environment)",
-  --             "providecommand",
-  --             "presetkeys",
-  --             "Declare%(Multi|Auto)?CiteCommand",
-  --             "Declare%(Index)?%(Field|List|Name)%(Format|Alias)",
-  --         },
-  --     },
-  --     cmd_single = {
-  --         cmds = { "hypersetup", "tikzset", "pgfplotstableread", "lstset" },
-  --     },
-  --     cmd_single_opt = {
-  --         cmds = { "usepackage", "includepdf" },
-  --     },
-  --     comments = {
-  --         enabled = 0,
-  --     },
-  --     env_options = vim.empty_dict(),
-  --     envs = {
-  --         blacklist = {},
-  --         whitelist = {
-  --             "figure",
-  --             "frame",
-  --             "table",
-  --             "example",
-  --             "answer",
-  --             "titlepage",
-  --             "cvskills",
-  --             "cventries",
-  --             "cvhonors",
-  --         },
-  --     },
-  --     items = {
-  --         enabled = 0,
-  --     },
-  --     markers = vim.empty_dict(),
-  --     preamble = {
-  --         enabled = 0,
-  --     },
-  --     sections = {
-  --         parse_levels = 0,
-  --         parts = { "appendix", "frontmatter", "mainmatter", "backmatter" },
-  --         sections = {
-  --             "%(add)?part",
-  --             "%(chapter|addchap)",
-  --             "%(section|section\\*)",
-  --             "%(subsection|subsection\\*)",
-  --             "%(subsubsection|subsubsection\\*)",
-  --             "paragraph",
-  --         },
-  --     },
-  -- }
+  vim.g.vimtex_fold_enabled = 1
+  vim.g.vimtex_fold_manual = 1
+  vim.g.vimtex_fold_types = {
+    cmd_addplot = {
+      cmds = { 'addplot[+3]?' },
+    },
+    cmd_multi = {
+      cmds = {
+        '%(re)?new%(command|environment)',
+        'providecommand',
+        'presetkeys',
+        'Declare%(Multi|Auto)?CiteCommand',
+        'Declare%(Index)?%(Field|List|Name)%(Format|Alias)',
+        'newtcbtheorem',
+        'newtcolorbox',
+      },
+    },
+    cmd_single = {
+      cmds = { 'hypersetup', 'tikzset', 'pgfplotstableread', 'lstset' },
+    },
+    cmd_single_opt = {
+      cmds = { 'usepackage', 'includepdf' },
+    },
+    comments = {
+      enabled = 0,
+    },
+    env_options = vim.empty_dict(),
+    envs = {
+      blacklist = {},
+      whitelist = {
+        'figure',
+        'frame',
+        'table',
+        'example',
+        'answer',
+        'titlepage',
+        'cvskills',
+        'cventries',
+        'cvhonors',
+        'align*',
+        'enumerate',
+        'itemize',
+        'tabular',
+      },
+    },
+    items = {
+      enabled = 0,
+    },
+    markers = vim.empty_dict(),
+    preamble = {
+      enabled = 0,
+    },
+    sections = {
+      parse_levels = 0,
+      parts = { 'appendix', 'frontmatter', 'mainmatter', 'backmatter' },
+      sections = {
+        '%(add)?part',
+        '%(chapter|addchap)',
+        '%(section|section\\*)',
+        '%(subsection|subsection\\*)',
+        '%(subsubsection|subsubsection\\*)',
+        'paragraph',
+      },
+    },
+  }
 end
