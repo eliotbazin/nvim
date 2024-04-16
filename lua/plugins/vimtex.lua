02b1d5ed1a94c3fe379b1e0eb11e8ed879d5b33a
Eliot Bazin <eliot.bazin@hotmail.com>
2024-04-16 16:01:14 +0200

    feat: disable quickfix open on compile

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -43,26 +43,26 @@ return function()
     styles = 1,
   }
   vim.g.vimtex_syntax_conceal_cites = { type = 'brackets', verbose = false }
-  -- vim.g.vimtex_quickfix_ignore_mode = 0
-  -- vim.g.vimtex_compiler_progname = "nvr"
-  -- -- PDF viewer settings
-  -- -- vim.g.vimtex_view_general_viewer = "SumatraPDF"
-  -- -- vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
-  -- -- Do not auto open quickfix on compile erros
-  -- vim.g.vimtex_quickfix_mode = 0
-  -- -- Latex warnings to ignore
-  -- vim.g.vimtex_quickfix_ignore_filters = {
-  --     "Command terminated with space",
-  --     "LaTeX Font Warning: Font shape",
-  --     "Package caption Warning: The option",
-  --     [[Underfull \\hbox (badness [0-9]*) in]],
-  --     "Package enumitem Warning: Negative labelwidth",
-  --     [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in]],
-  --     [[Package caption Warning: Unused \\captionsetup]],
-  --     "Package typearea Warning: Bad type area settings!",
-  --     [[Package fancyhdr Warning: \\headheight is too small]],
-  --     [[Underfull \\hbox (badness [0-9]*) in paragraph at lines]],
-  --     "Package hyperref Warning: Token not allowed in a PDF string",
+
+  -- Disable quickfix auto open
+  vim.g.vimtex_quickfix_ignore_mode = 1
+  -- Auto open quickfix on compile erros
+  vim.g.vimtex_quickfix_mode = 1
+  -- Latex warnings to ignore
+  vim.g.vimtex_quickfix_ignore_filters = {
+    'Command terminated with space',
+    'LaTeX Font Warning: Font shape',
+    'Package caption Warning: The option',
+    [[Underfull \\hbox (badness [0-9]*) in]],
+    'Package enumitem Warning: Negative labelwidth',
+    [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in]],
+    [[Package caption Warning: Unused \\captionsetup]],
+    'Package typearea Warning: Bad type area settings!',
+    [[Package fancyhdr Warning: \\headheight is too small]],
+    [[Underfull \\hbox (badness [0-9]*) in paragraph at lines]],
+    'Package hyperref Warning: Token not allowed in a PDF string',
+    [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in paragraph at lines]],
+  }
   --     [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in paragraph at lines]],
   -- }
   -- vim.g.vimtex_fold_enabled = 1
